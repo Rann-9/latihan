04bf02e71e376989f84a795138f2e58d2b3b975e
Muhammad Jahran <rann020070.com>
2024-02-13 14:53:24 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<style>
+    nav {
+        display: flex;
+        
+    }
+</style>
+
+<body>
+    <nav>
+        <h1>Rann</h1>
+        <ul>
+            <li>
+                Home
+            </li>
+            <li>
+                About
+            </li>
+        </ul>
+    </nav>
+</body>
+</html>
